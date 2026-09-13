0a6222f773c841564ea32a7a7ca27acacb9d5a8f
Isaam Shah <wonderboyisu@gmail.com>
2026-09-13 19:32:52 -0400

    Fixed a bug: delete_task always removed the first task instead of the specified index

--- task_manager.py
+++ task_manager.py
@@ -23,8 +23,8 @@ class TaskManager:
         for index, task in enumerate(self.tasks, start=1):
             print(f"Task {index}: {task}")
 
-    def delete_task(self, index):
-        if self.tasks:
-            self.tasks.pop(0)
-            self.save_tasks()
+def delete_task(self, index):
+    if self.tasks and 0 <= index < len(self.tasks):
+        self.tasks.pop(index)
+        self.save_tasks()
 
