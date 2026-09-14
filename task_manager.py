70e0a028405a796c4a0ab8abc223f0ef6a6874ae
Isaam Shah <wonderboyisu@gmail.com>
2026-09-13 22:42:08 -0400

    Revert "Bug: delete_task always removes the first task"
    This reverts commit 9ee44808b6a15306ae07c45bbd48922f3d13055b.

--- task_manager.py
+++ task_manager.py
@@ -23,9 +23,11 @@ class TaskManager:
         for index, task in enumerate(self.tasks, start=1):
             print(f"Task {index} *** {task}")
 
+
+
     def delete_task(self, index):
         if self.tasks and 0 <= index < len(self.tasks):
-            self.tasks.pop(0)
+            self.tasks.pop(index)
             self.save_tasks()
     
     def complete_task(self, index):
